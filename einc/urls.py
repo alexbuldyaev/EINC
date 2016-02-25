9dd714afa251d7c6157cfce7366862493c62c568
alexbuldyaev <alexbuldyaev@gmail.com>
2016-02-25 13:15:49 +0300

    myform

--- einc/urls.py
+++ einc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^count/', views.post_count, name='post_count'),
 ]
--- einc/views.py
+++ einc/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def post_list(request):
-    return render(request, 'einc/post_list.html', {})
+    return render(request, 'einc/results.html', {})
+
+def post_count(request):
+    return render(request, 'einc/count.html', {})
