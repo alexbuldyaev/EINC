ec6dcd37cefc4667b91b3fcad64d680cf9e0fa40
alexbuldyaev <alexbuldyaev@gmail.com>
2016-02-25 15:53:23 +0300

    counters

--- einc/urls.py
+++ einc/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^count/', views.post_count, name='post_count'),
+    url(r'^countosg/', views.post_count, name='post_count'),
+    url(r'^countksk/', views.post_countksk, name='post_countksk'),
 ]
