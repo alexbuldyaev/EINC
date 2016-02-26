b62c84f3afec1d77aa5762255c090d66db1dba90
alexbuldyaev <alexbuldyaev@gmail.com>
2016-02-27 00:37:59 +0300

    new pages

--- einc/views.py
+++ einc/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
     return render(request, 'einc/results.html', {})
@@ -9,3 +11,9 @@ def post_countksk(request):
     return render(request, 'einc/countksk.html', {})
 def post_counthom(request):
     return render(request, 'einc/counthome.html', {})
+def post_signin(request):
+    return render(request, 'einc/signin.html', {})
+def post_bepay(request):
+    return render(request, 'einc/bepay.html', {})
+
+
